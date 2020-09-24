f854332a08280f88aa2d770cbca46b6138327416
Irene Smith <irenesmith13@gmail.com>
2020-09-24 09:03:07 -0400

    updated all files for Fall 2020

--- guidelines.php
+++ guidelines.php
@@ -1,96 +1,180 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Submission Guidelines for Nimue's Grotto">
-	<title>Nimue's Grotto - Guidelines</title>
-	<link href="https://fonts.googleapis.com/css?family=Griffy" rel="stylesheet">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/site.css">
-	<link rel="icon" type="image/png" href="img/favicon.png">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Submission Guidelines for Nimue's Grotto">
+  <title>Nimue's Grotto - Guidelines</title>
+  <link href="https://fonts.googleapis.com/css?family=Griffy" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/site.css">
+  <link rel="icon" type="image/png" href="img/favicon.png">
 </head>
+
 <body>
-	<?php include 'partials/header.html' ?>
+  <?php include 'partials/header.html' ?>
 
-	<main class="container-fluid">
-		<section class="row">
-			<?php $active = 'guidelines' ?>
-			<?php include 'partials/issue-menu.html' ?>
-			<section class="col-md-6">
-				<article>
-					<h1>Submission Guidelines</h1>
-					<p>We are currently seeking submissions for the Fall, 2020 Issue of Nimue’s Grotto. The deadline for submissions to the Fall, 2020 issue is October 15, 2020.</p>
+  <main class="container-fluid">
+    <section class="row">
+      <?php $active = 'guidelines' ?>
+      <?php include 'partials/issue-menu.html' ?>
+      <section class="col-md-6">
+        <article>
+          <h1>Submission Guidelines</h1>
+          <p>
+            We are currently seeking submissions for the Fall, 2020 Issue of
+            Nimue’s Grotto. The deadline for submissions is October 24, 2020.
+          </p>
 
-					<h2>Do you write flash fiction?</h2>
+          <h2>What are we looking for?</h2>
 
-					<p>If so, we want to hear from you! We are looking for flash fiction in the genres of Fantasy, Science Fiction, and Horror. Stories of 1,000 words or less, please! There’s no lower limit. If you can tell a complete story in twenty-five words, go for it!</p>
+          <p>
+            <strong><em>We've updated our guidlines!</em></strong> Starting
+            with the Fall 2020 Issue, we are looking for book and movie reviews
+            in addition to fiction.
+          </p>
 
-					<p>What are we looking for?</p>
+          <p>
+            We are looking for flash fiction in the genres of Fantasy, Science
+            Fiction, and Horror. Stories of 1,000 words or less, please!
+            There’s no lower limit. If you can tell a complete story in
+            twenty-five words, go for it!
+          </p>
 
-					<p>As I said above, we want Fantasy, Science Fiction, and Horror stories that are up to 1,000 words long.</p>
+          <p>
+            We want complete stories. In other words, stories with interesting
+            characters, and stories that have a beginning, a middle, and an
+            end.
+          </p>
 
-					<p>We want stories. In other words, complete stories with interesting characters, and stories that have a beginning, a middle, and an end.</p>
+          <p>
+            Entertain us. Make us laugh or cry, and best of all, make us think!
+          </p>
 
-					<p>Entertain us. Make us laugh or cry, and best of all, make us think!</p>
+          <p>
+            We want First Electronic Rights. This means that we wish to publish
+            your story first on our website. After six months, you are free to
+            publish the story again anywhere you wish. We only ask that you
+            credit Nimue’s Grotto as the first publisher. If you publish again
+            online, we also ask that you include a link to the magazine’s main
+            page at:
+            <a ahref="https://nimuesgrotto.com">https://nimuesgrotto.com</a>.
+          </p>
 
-					<p>We wish to purchase First Electronic Rights. This means that we wish to publish your story first on our website. After the next issue has been published, you are free to publish the story again anywhere you wish. We only ask that you credit Nimue’s Grotto as the first publisher. If you publish again online, we also ask that you include a link to the magazine’s main page at: http://nimuesgrotto.com.</p>
+          <h3>Non-fiction</h3>
 
-					<p>Unfortunately, we are not a paying market right now. We hope that will change sometime in the future, but for now all we can do is say, “Thank you!”</p>
+            <p>
+              <strong>Something new!!</strong> Starting with Fall 2020, we are not
+              accepting book and movie reviews but the genre of the book or movie
+              must match the genres we publish. In other words, Horror, Fantasy,
+              Science Fiction, or Fantasy.<br> Send us a query to
+              <a href="mailto:editor@nimuesgrotto.com">editor@nimuesgrotto.com</a>,
+              just in case we are already covering it.
+            </p>
 
-					<h2>What do we NOT want?</h2>
+            <p>
+              Unfortunately, we are not a paying market right yet. We hope that
+              will change sometime in the future, but for now all we can do is
+              say, “Thank you!” And publish your story, of course.
+            </p>
 
-					<p>We’re pretty flexible when it comes to fiction, but there are some things we are definitely notinterested in seeing:</p>
+            <h2>What we <strong>DO NOT</strong> Want</h2>
 
-					<ul>
-						<li>Genres other than those listed above.<br>
+            <p>
+              We’re pretty flexible when it comes to fiction, but there are some
+              things we are definitely notinterested in seeing:
+            </p>
 
-						In other words, we don’t want Contemporary Romance, Mystery, or Literary stories. Unless, of course, you can combine Romance and Fantasy or Mystery and Science Fiction, for example. Oh, and by “Fantasy” I mean magic, elves, and that kind of thing, not what you wish your spouse would do to you on a romantic get away. Which leads me to. . .<li>
+            <ul>
+              <li>Genres other than those listed above.<br>
 
-						<li>Erotica<br>
+                In other words, we don’t want Contemporary Romance, Mystery, or
+                Literary stories. Unless, of course, you can combine Romance and
+                Fantasy or Mystery and Science Fiction, for example. Oh, and by
+                “Fantasy” I mean magic, elves, and that kind of thing, not what you
+                wish your spouse would do to you on a romantic get away. Which
+                leads me to. . .</li>
 
-						I love to read all kinds of stories, but we do not want stories that are mainly about sexual interactions, even between consenting adults.</li>
+              <li>Erotica<br>
 
-					<li>Non-fiction<br>
+                I love to read all kinds of stories, but we do not want stories
+                that are mainly about sexual interactions, even between consenting
+                adults.</li>
 
-					We are not interested in non-fiction , movie reviews, book reviews, or interviews with the rich and famous. On the other hand, if you want to write non-fiction, and your topic is writing, the writing life, or self-publishing, consider submitting to “Dreams in Transit,” an online eZine for writers who write about writing. (Details on “Dreams in Transit” to follow.)</li>
+              <li>Reprints<br>
 
-					<li>Reprints<br>
+                If you have published your story somewhere else, we don’t want it.</li>
 
-					If you have published your story somewhere else, we don’t want it.</li>
+            </ul>
 
-					</ul>
+            <h2>How to Submit</h2>
+            <h3>Fiction</h3>
+            <p>
+              To submit a story for consideration, please paste it in the body
+              of an email message, at the end of the story put the following:
+            </p>
 
-					<h2>How to Submit</h2>
+            <p>— The End —</p>
 
-					<p>To submit a story for consideration, please paste it in the body of an email message, at the end of the story put the following:</p>
+            <p>
+              and follow it with one (short) paragraph telling us a little bit
+              about yourself. You can include links to your own website, or your
+              publisher’s website.
+            </p>
 
-					<p>— The End —</p>
+            <p>Make the subject something like this:</p>
 
-					<p>and follow it with one paragraph telling us a little bit about yourself. You can include links to your own website, or your publisher’s website.</p>
+            <p>SUBMISSION: story_title (e.g. SUBMISSION: Lost and Found)</p>
 
-					<p>Make the subject something like this:</p>
+            <p>Send your submission to:</p>
 
-					<p>SUBMISSION: story_title (e.g. SUBMISSION: Lost and Found)</p>
+            <p>editor@nimuesgrotto.com</p>
 
-					<p>Send your submission to:</p>
+            <h3>Reviews</h3>
 
-					<p>editor@nimuesgrotto.com</p>
-				</article>
-			</section>
-		</section>
-	</main>
+            <p>
+              Reviews are the only type of non-fiction we are consdering right
+              now. Reviews of books and movies don't necessarily have to be
+              newly released. Movies in theaters are great but movies just
+              released to Blu-Ray/DVD are also acceptable.
+            </p>
 
-	<footer class="container-fluid">
-		<div class="col-md-12">
-			<p class="text-center">Nimue's Grotto Copyright &copy; 2017, Irene P. Smith. All rights reserved.</p>
-			<p class="text-center">Read our <a href="privacy.html">privacy policy</a>.</p>
-		</div>
-	</footer>
+            <p>
+              However, please let us know what book or movie you would like to
+              review. I'd hate for you to waste your time reviewing something
+              that has already been accepted. Send an email with a subject line
+              something like this:
+            </p>
 
-	<!-- End of Content -->
-	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+            <p>
+              REVIEW: book_or_movie_title (e.g. REVIEW: Onward)
+            </p>
+
+            <p>
+              And again, send your proposal to:
+            </p>
+
+            <p>
+              editor@nimuesgrotto.com
+            </p>
+
+        </article>
+      </section>
+    </section>
+  </main>
+
+  <footer class="container-fluid">
+    <div class="col-md-12">
+      <p class="text-center">Nimue's Grotto Copyright &copy; 2017, Irene P. Smith. All rights reserved.</p>
+      <p class="text-center">Read our <a href="privacy.html">privacy policy</a>.</p>
+    </div>
+  </footer>
+
+  <!-- End of Content -->
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 </body>
+
 </html>
